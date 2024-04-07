9d02decd4bb8b4cb1b1a3db092fd1396792e00e3
Harriet_atieno <66387509+Harrieguru@users.noreply.github.com>
2024-04-07 19:44:55 -0400

    Add files via upload

---
+++ aspWebapp/Pages/DatabaseAccess.cs
@@ -0,0 +1,10 @@
+﻿namespace aspWebapp.Pages
+{
+    public class DatabaseAccess
+    {
+        public void OnGet()
+        {
+            Console.WriteLine("Database access has already been done on April 14, 2023 by Harriet Odhiambo");
+        }
+    }
+}
